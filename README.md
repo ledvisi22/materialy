c2ac34cd201616e2c57810d741b4c8e9195d5409
Jakub Vančata <jakub.vancata@moneta.cz>
2025-06-20 18:45:40 +0200

    first CTF

--- README.md
+++ README.md
@@ -6,5 +6,16 @@ Node v24
 # Docker
 `docker compose build && docker compose up`
 
+## Single dockerfile build and run
+use the docker commands in package.json
+`npm run docker:build && npm run docker:run`
+
+or run it manually
+
+`docker build -t your-tag .`
+`docker run -p 127.0.0.1:3000:3000 your-tag`
+
+
+
 ## Docker on server
 `docker compose build && docker compose up --detach`
---
+++ apps/ctf-1/index.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json());
+
+app.post('/', cors(), (request, response) => {
+    const input = request.body.input;
+
+    if (!input || !input.length) {
+        response.send('Musíš poslat POST request s tělem { "input": "string" }. Maximální délka vstupu je pět znaků.');
+        return;
+    };
+
+    if (input.length > 5) {
+        response.send(`Sorry, input je delší než pět znaků. Tvůj vstup je "${input}"`);
+        return;
+    }
+
+    if (input.toString().length <= 5) {
+        response.send(`Sorry, musíš poslat input delší než pět znaků. Tvůj vstup je "${input}"`);
+        return;
+    }
+
+    response.send(`Gratuluju 🚩. Tvůj vstup je "${input}"`);
+    return;
+})
+
+app.listen(port, () => {
+    console.log(`ctf-1 running on port ${port}`)
+})
