57e8b7ba351f12dea8c2944ec5334125112ad083
Radek Titěra <50217333+Rodak123@users.noreply.github.com>
2025-09-30 14:25:23 +0200

    add: Jet2 Holiday Sound (#9)
    Co-authored-by: Jakub Vančata <vancata.kuba@seznam.cz>

--- apps/soundboard-api/const.ts
+++ apps/soundboard-api/const.ts
@@ -22,5 +22,6 @@ export const sounds: Sound[] = [
     { url: "sounds/phone.mp3", name: "Phone" },
     { url: "sounds/rizz-sound-effect.mp3", name: "Rizz" },
     { url: "sounds/cr-brrr.mp3", name: "CR brrr" },
+    { url: "sounds/jet2.mp3", name: "Jet 2 Holiday" },
     { url: "sounds/bonk.mp3", name: "Bonk" },
 ]
