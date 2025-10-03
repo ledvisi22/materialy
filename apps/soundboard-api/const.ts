4bf64e3b0b09e85c673176baaa9a7d16f192d6c7
Jan Kynčl <145004616+kyncl@users.noreply.github.com>
2025-10-03 09:27:43 +0200

    Adding vypecky (#15)

--- apps/soundboard-api/const.ts
+++ apps/soundboard-api/const.ts
@@ -27,5 +27,6 @@ export const sounds: Sound[] = [
     { url: "sounds/deezNuts.mp3", name: "deezNuts" },
     { url: "sounds/hihihi-haw.mp3", name: "Hihihi Haw" },
     { url: "sounds/mimimimi.mp3", name: "mimimimi" },
-    { url: "sounds/bomboclat.mp3", name: "bomboclat"},
+    { url: "sounds/bomboclat.mp3", name: "bomboclat" },
+    { url: "sounds/vypecky.mp3", name: "vypecky" },
 ]
